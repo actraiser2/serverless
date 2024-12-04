4066a210f92c85489fef2940e8e1d5eface2dde3
jmbesada <jmbesada.juez@gmail.com>
2024-12-04 19:06:49 +0100

    @MockBean for S3Client in the tests

--- src/test/java/com/santalucia/serverless/ServerlessApplicationTests.java
+++ src/test/java/com/santalucia/serverless/ServerlessApplicationTests.java
@@ -2,10 +2,15 @@ package com.santalucia.serverless;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class ServerlessApplicationTests {
 
+	@MockBean S3Client s3Client;
+	
 	@Test
 	void contextLoads() {
 	}
